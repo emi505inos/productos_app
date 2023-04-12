598ecf6e91c18307e43b59c73c13bcc286a3063d
emi505inos <emi.inos505>
2023-04-12 18:10:47 -0300

    modified:   lib/widgets/product_card.dart 	modified:   pubspec.yaml

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -10,13 +10,28 @@ class ProductCard extends StatelessWidget{
         width: double.infinity,
         height: 400,
         decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+            _ProductDetails(),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: _PriceTag()),
+            Positioned(
+              top: 0,
+              left: 0,
+              child: _NotAvailable()),
+        ]
+        ),
         
       ),
     );
   }
 
   BoxDecoration _cardBorders() => BoxDecoration(
-    color: Colors.red,
+    color: Colors.white,
     borderRadius: BorderRadius.circular(25),
     boxShadow: [
       BoxShadow(
@@ -26,4 +41,118 @@ class ProductCard extends StatelessWidget{
     )
     ]
   );
+}
+
+class _NotAvailable extends StatelessWidget {
+  const _NotAvailable({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No Disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+        color: Colors.yellow[800],
+      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), topRight: Radius.circular(25))
+
+      ),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text('\$103.99', style: TextStyle(color: Colors.white, fontSize: 20),)),
+      ),
+      width: 100,
+      height: 70,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+        color: Colors.indigo,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), topRight: Radius.circular(25)) 
+      ),
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 50),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Disco Duro G', 
+              style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'id del disco Duro', 
+              style: TextStyle(fontSize: 15, color: Colors.white),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+    color: Colors.indigo,
+    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25), topRight: Radius.circular(25))
+  );
+}
+
+class _BackgroundImage extends StatelessWidget {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
+        
+      ),
+    );
+  }
 }
