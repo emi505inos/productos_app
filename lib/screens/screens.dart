8afe6edfd93a05d3fd1203f98ad2374a1dc1a1cb
emi505inos <emi.inos505>
2023-04-12 14:59:36 -0300

    first commit

---
+++ lib/screens/screens.dart
@@ -0,0 +1,6 @@
+import 'package:productos_app/screens/home_screen.dart';
+import 'package:productos_app/screens/login_screen.dart';
+
+
+export 'package:productos_app/screens/home_screen.dart';
+export 'package:productos_app/screens/login_screen.dart';
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,5 @@
+import 'package:productos_app/widgets/auth_background.dart';
+import 'package:productos_app/widgets/card_container.dart';
+
+export 'package:productos_app/widgets/auth_background.dart';
+export 'package:productos_app/widgets/card_container.dart';
