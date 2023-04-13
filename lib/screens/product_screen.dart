752596a1e08c88552a7f0ba56fce8b0251a7e27e
emi505inos <emi.inos505>
2023-04-13 11:28:11 -0300

    modified:   lib/screens/product_screen.dart 	modified:   lib/widgets/product_imagen.dart 	modified:   lib/widgets/widgets.dart

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/widgets/widgets.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 
 class ProductScreen extends StatelessWidget {
   @override
@@ -9,11 +11,90 @@ class ProductScreen extends StatelessWidget {
           children: [
             Stack(
               children: [
-                
+                ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    onPressed:() => Navigator.of(context).pop(),
+                    icon: const Icon(Icons.arrow_back_ios_new, size: 40, color: Colors.white,)),
+                ),
+                Positioned(
+                  top: 60,
+                  right: 25,
+                  child: IconButton(
+                    onPressed:() {},
+                    icon: const Icon(Icons.camera_alt_outlined, size: 40, color: Colors.white,)),
+                ),
               ],
-            )
+            ),
+            _ProductForm(),
+           const SizedBox(height: 100,),
+
           ],
         )),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.save_outlined),
+          onPressed: () {
+            
+          },),
+    );
+  }
+}
+
+class _ProductForm extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        child: Form(
+          child: Column(
+            children: [
+              const SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: 'Nombre del Producto',
+                  labelText: 'Nombre:', 
+                ),
+              ),
+              const SizedBox(height: 30,),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecoration(
+                  hintText: '\$150',
+                  labelText: 'Precio:', 
+                ),
+              ),
+              const SizedBox(height: 30,),
+              SwitchListTile.adaptive(
+                value: true, 
+                title: Text('Disponible'),
+                activeColor: Colors.indigo,
+                onChanged: (value) {
+                  
+                },)
+            ],
+          )),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() {
+    return const BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.only(bottomLeft: Radius.circular(25),bottomRight: Radius.circular(25)),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black12,
+          offset: Offset(0,5),
+          blurRadius: 5
+        )] 
     );
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,7 +1,9 @@
 import 'package:productos_app/widgets/auth_background.dart';
 import 'package:productos_app/widgets/card_container.dart';
 import 'package:productos_app/widgets/product_card.dart';
+import 'package:productos_app/widgets/product_imagen.dart';
 
 export 'package:productos_app/widgets/auth_background.dart';
 export 'package:productos_app/widgets/card_container.dart';
 export 'package:productos_app/widgets/product_card.dart';
+export 'package:productos_app/widgets/product_imagen.dart';
