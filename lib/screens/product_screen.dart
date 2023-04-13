30021e8ce763e54c71ffe2709ddc1f89c9b06665
emi505inos <emi.inos505>
2023-04-13 10:12:37 -0300

    modified:   lib/screens/product_screen.dart 	new file:   lib/widgets/product_imagen.dart

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -4,8 +4,16 @@ class ProductScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(
-        child: Text('Product Screen') ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                
+              ],
+            )
+          ],
+        )),
     );
   }
 }
